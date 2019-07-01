135c0eb5802b11d245e7b15d75f6c23c9989079f
u001djn <david.novogrodsky@carlsonwagonlit.com>
2019-07-01 18:22:32 -0500

    first test

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  // Which component to show?
+  // send the answer out as output
+  @Output() recipes: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+
+
+  showRecipes() {
+    this.recipes.emit(true);
+  }
 }
